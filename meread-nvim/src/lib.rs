ea32eaa97531d9c4cf6f3fb7f0ae84a29b1555f8
Samuel Åkesson <sermuns@lysator.liu.se>
2026-06-30 13:31:12 +0200

    create skeleton `setup` for plugin

--- meread-nvim/src/lib.rs
+++ meread-nvim/src/lib.rs
@@ -1,4 +1,23 @@
+use nvim_oxi::api::{self, opts::*, types::*};
+use nvim_oxi::{Dictionary, Function, Object, print};
+
 #[nvim_oxi::plugin]
-fn foo() -> i32 {
-    42
+fn meread() -> Dictionary {
+    Dictionary::from_iter([("setup", Function::from_fn(setup))])
+}
+
+fn setup(_args: Object) {
+    let opts = CreateCommandOpts::builder()
+        .bang(true)
+        .desc("shows a greetings message")
+        .nargs(CommandNArgs::ZeroOrOne)
+        .build();
+
+    let greetings = |args: CommandArgs| {
+        let who = args.args.unwrap_or("from Rust".to_owned());
+        let bang = if args.bang { "!" } else { "" };
+        print!("Hello {}{}", who, bang);
+    };
+
+    api::create_user_command("Greetings", greetings, &opts).unwrap();
 }
